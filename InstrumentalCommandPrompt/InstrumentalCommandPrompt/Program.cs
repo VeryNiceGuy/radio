5f7f02264b12c17979c5b55f8bc74969b05c6d98
Unknown <killingmesoftlywithchainsaw@gmail.com>
2017-10-26 15:11:10 -0700

    Terminal
    Desktop app to connect to zedboard over SSH.

---
+++ InstrumentalCommandPrompt/InstrumentalCommandPrompt/Program.cs
@@ -0,0 +1,34 @@
+﻿using Renci.SshNet;
+using System;
+
+namespace InstrumentalCommandPrompt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter IP:");
+            string ip = Console.ReadLine();
+
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+
+            using (var client = new SshClient(ip, username, password))
+            {
+                client.Connect();
+
+                while (true)
+                {
+                    var input = Console.ReadLine();
+                    if (input.Contains("exit instrumental command prompt"))
+                        break;
+
+                    Console.WriteLine(client.RunCommand(input).Result);
+                }
+            }
+        }
+    }
+}
